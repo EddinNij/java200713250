d5a91a56b15e6713b6cc3e33f431f439033d7c47
EddinNij <rockeistars@gmail.com>
2020-10-07 14:08:09 -0600

    repositorio de java

---
+++ ejemplo3/src/ejemplo3/Ejemplo3.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplo3;
+
+/**
+ *
+ * @author Zeqi
+ */
+public class Ejemplo3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a=1;
+        int b=2;
+        int m=a*b;
+        System.out.println("La multiplicación es " + m);
+    }
+    
+}
